ebb5187c9e86c9138ccf258f1f2c6df404398c48
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-07-19 16:10:42 +0530

    Create Admin Dashboard

---
+++ src/pages/AdminDashboard/index.jsx
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import {Button, Grid, TextField} from "@mui/material";
+import {withStyles} from "@mui/styles";
+import {styleSheet} from "../CustomerDashboard/styles";
+import logo from "../../assets/img/logo.png";
+import rev1 from "../../assets/img/rev1.jpg";
+
+
+const drawerWidth = 240;
+
+function AdminDashboard(props) {
+    const {window} = props;
+    const {classes} = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const drawer = (
+        <div>
+            <Toolbar/>
+            <Divider/>
+            <List>
+                {['General', 'Premium', 'Luxury','Profile'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider/>
+        </div>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+    return (
+        <Box sx={{display: 'flex'}}>
+
+            <Box
+                component="nav"
+                sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
+                aria-label="mailbox folders"
+            >
+                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: {xs: 'block', sm: 'none'},
+                        '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+                <Drawer
+                    variant="permanent"
+                    sx={{
+                        display: {xs: 'none', sm: 'block'},
+                        '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
+                    }}
+                    open
+                >
+                    {drawer}
+                </Drawer>
+            </Box>
+            <Grid className={classes.container}>
+                
+            </Grid>
+        </Box>
+    );
+}
+
+AdminDashboard.propTypes = {
+    window: PropTypes.func,
+};
+
+export default withStyles(styleSheet)(AdminDashboard);
