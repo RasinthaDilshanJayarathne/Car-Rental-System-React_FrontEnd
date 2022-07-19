8ffef43869f2620c14227c13aae2d66722c46461
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-07-19 19:38:09 +0530

    Customer Manage Page

---
+++ src/pages/AdminDashboard/CustomerManage/index.jsx
@@ -0,0 +1,18 @@
+import { withStyles } from "@mui/material";
+import { styleSheet } from "./styles";
+
+const { Component } = require("react");
+
+class CustomerManage extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <Grid></Grid>
+        )
+    }
+}
+
+export default withStyles(styleSheet)(CustomerManage)
