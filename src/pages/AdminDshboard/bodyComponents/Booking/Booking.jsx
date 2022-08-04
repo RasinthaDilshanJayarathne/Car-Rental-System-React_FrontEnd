81769ecbb1d752ca107d620ccfa6689ca46efcec
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-04 15:19:45 +0530

    Updating

--- src/pages/AdminDshboard/bodyComponents/Booking/Booking.jsx
+++ src/pages/AdminDshboard/bodyComponents/Booking/Booking.jsx
@@ -16,42 +16,110 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import EditIcon from '@mui/icons-material/Edit';
+import BookingService from '../../../../Service/BookingService'
 
 class Booking extends Component {
     constructor(props) {
         super(props)
-        this.state ={
-            formData :{
-                rentList:[],
-                
-            }
+        this.state = {
+
+            formData: {
+                rentId: "",
+                pickUpDate: "",
+                pickUpTime: "",
+                returnDate: "",
+                driverRequestingType: "",
+                location: "",
+                customer: {
+                    id: '',
+                    nic: '',
+                    name: {
+                        firstName: '',
+                        lastName: ''
+                    },
+                    licenseNo: '',
+                    address: '',
+                    contactNo: '',
+                    email: '',
+                    user: {
+                        userName: '',
+                        password: '',
+                        role: ''
+                    }
+                },
+                bookingDetails: {
+                    vehicleId: '',
+                    rentId: '',
+                    vehicle: {
+                        vehicleId: '',
+                        registrationNo: '',
+                        vehicleBrand: '',
+                        numberOfPassengers: '',
+                        color: '',
+                        vehicleType: '',
+                        fuelType: '',
+                        transmissionType: '',
+                        freeMileage: {
+                            dailyMileage: '',
+                            monthlyMileage: ''
+                        },
+                        priceRate: {
+                            dailyRate: '',
+                            monthlyRate: ''
+                        },
+                        vehicleAvailabilityType: '',
+                        damageFee: '',
+                        lastServiceMileage: '',
+                        vehicleServiceMileage: '',
+                        pricePerExtraKM: '',
+                        totalVehicle: ''
+                    },
+                },
+                driverSchedules: {
+                    driverId: '',
+                    rentId: '',
+                    driver: {
+                        id: '',
+                        nic: '',
+                        name: {
+                            firstName: '',
+                            lastName: ''
+                        },
+                        licenseNo: '',
+                        address: '',
+                        contactNo: '',
+                        email: '',
+                        user: {
+                            userName: '',
+                            password: '',
+                            role: ''
+                        },
+                        driverAvailability: ''
+                    }
+                },
+            },
+            data:[],
         }
+    }
+
+    loadData = async () => {
+        let res = await BookingService.fetchBooking();
+
+        if (res.status === 200) {
+            this.setState({
+                data: res.data.data
+            });
+        }
+        console.log(this.state.data)    // print customers array
+
+    };
+
+    componentDidMount() {
+        this.loadData();
     }
 
     render() {
         const { classes } = this.props;
-
-        function createData(id, email, firstName, lastName, licenNo, contact, nic, address) {
-            return { id, email, firstName, lastName, licenNo, contact, nic, address };
-        }
-
-        const rows = [
-            createData('R00-001', 'rasintha@gmail.com', 'Nimal', 'Perera', 2423452, '071-3123342', '42345124', 'Galle'),
-            createData('R00-002', 'rasintha@gmail.com', 'Kamal', 'Gamage', 2373453, '071-3123342', '42345124', 'Colombo'),
-            createData('R00-003', 'rasintha@gmail.com', 'Amal', 'Soisa', 2624567, '071-3123342', '42345124', 'Panadura'),
-            createData('R00-004', 'rasintha@gmail.com', 'Nadun', 'Hettiarachci', 3053434, '071-3123342', '42345124', 'Matara'),
-            createData('R00-005', 'rasintha@gmail.com', 'Wimal', 'Gamage', 3562432, '071-3123342', '42345124', 'Galle'),
-            createData('R00-006', 'rasintha@gmail.com', 'Rasintha', 'Perera', 1596756, '071-3123342', '42345124', 'Matara'),
-            createData('R00-007', 'rasintha@gmail.com', 'Yasiru', 'Soisa', 237876, '071-3123342', '42345124', 'Panadura'),
-            createData('R00-008', 'rasintha@gmail.com', 'Minura', 'Hettiarachci', 2627564, '071-3123342', '42345124', 'Matara'),
-            createData('R00-009', 'rasintha@gmail.com', 'Yasidu', 'Soisa', 3055632, '071-3123342', '42345124', 'Colombo'),
-            createData('R00-0010', 'rasintha@gmail.com', 'Upali', 'de Silva', 3561233, '071-3123342', '42345124', 'Panadura'),
-            createData('R00-0011', 'rasintha@gmail.com', 'Sunil', 'Hettiarachci', 1593546, '071-3123342', '42345124', 'Colombo'),
-            createData('R00-0012', 'rasintha@gmail.com', 'Supun', 'Perera', 237978, '071-3123342', '42345124', 'Matara'),
-            createData('R00-0013', 'rasintha@gmail.com', 'Kanthi', 'Gamage', 2622345, '071-3123342', '42345124', 'Colombo'),
-            createData('R00-0014', 'rasintha@gmail.com', 'Iresha', 'Hettiarachci', 305578, '071-3123342', '42345124', 'Galle'),
-            createData('R00-0015', 'rasintha@gmail.com', 'Naduni', 'Perera', 3565673, '071-3123342', '42345124', 'Matara'),
-        ];
 
         return (
             <Grid className={classes.container}>
@@ -64,12 +132,12 @@ class Booking extends Component {
                     </Grid>
                 </Grid>
                 <Grid className={classes.subContainer}>
-                    
+
                     <Grid className={classes.table2}>
-                        <Typography variant="h5" style={{ marginLeft: '30px', color: '#95a5a6',marginTop:'10px'}}>
+                        <Typography variant="h5" style={{ marginLeft: '30px', color: '#95a5a6', marginTop: '10px' }}>
                             VIEW ALL BOOKING DETAILS
                         </Typography>
-                        <TableContainer component={Paper} style={{ height: '77vh', width: '80vw', backgroundColor: '#eeeff1',marginTop:'-100px'}}>
+                        <TableContainer component={Paper} style={{ height: '77vh', width: '80vw', backgroundColor: '#eeeff1', marginTop: '-100px' }}>
                             <Table aria-label="simple table">
                                 <TableHead>
                                     <TableRow>
@@ -77,37 +145,58 @@ class Booking extends Component {
                                         <TableCell align="left">Pick-Up-Date</TableCell>
                                         <TableCell align="left">Pick-Up-Time</TableCell>
                                         <TableCell align="left">Return Date</TableCell>
+                                        <TableCell align="left">Location</TableCell>
                                         <TableCell align="left">Driver Requesting Type</TableCell>
                                         <TableCell align="left">Driver Id</TableCell>
                                         <TableCell align="left">Driver Name</TableCell>
                                         <TableCell align="left">Customer Id</TableCell>
                                         <TableCell align="left">Customer Name</TableCell>
-                                        <TableCell align="left">Driver Id</TableCell>
-                                        <TableCell align="left">Customer License No</TableCell>
-                                        <TableCell align="left">Customer Address</TableCell>
-                                        <TableCell align="left">Customer Contact No</TableCell>
-                                        <TableCell align="left">Customer E-mail</TableCell>
+                                        <TableCell align="left">Vehicle Id</TableCell>
+                                        <TableCell align="left">Damage Fee</TableCell>
                                         <TableCell align="left">Action</TableCell>
 
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {
-                                    rows.map((row) => (
-                                        <TableRow
-                                            key={row.name}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                        >
-                                            <TableCell align="left">{row.id}</TableCell>
-                                            <TableCell align="left">{row.email}</TableCell>
-                                            <TableCell align="left">{row.firstName}</TableCell>
-                                            <TableCell align="left">{row.lastName}</TableCell>
-                                            <TableCell align="left">{row.licenNo}</TableCell>
-                                            <TableCell align="left">{row.contact}</TableCell>
-                                            <TableCell align="left">{row.nic}</TableCell>
-                                            <TableCell align="left">{row.address}</TableCell>
-                                        </TableRow>
-                                    ))}
+                                {/* {
+                                        this.state.data.map((row) => (
+                                            <TableRow
+                                                key={row.name}
+                                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                            >
+                                                <TableCell align="left">{row.rentId}</TableCell>
+                                                <TableCell align="left">{row.pickUpDate}</TableCell>
+                                                <TableCell align="left">{row.pickUpTime}</TableCell>
+                                                <TableCell align="left">{row.returnDate}</TableCell>
+                                                <TableCell align="left">{row.location}</TableCell>
+                                                <TableCell align="left">{row.driverRequestingType}</TableCell> 
+                                                <TableCell align="left">{row.driverSchedules.driver.id}</TableCell>
+                                                <TableCell align="left">{row.driverSchedules.driver.name}</TableCell>
+                                                <TableCell align="left">{row.customer.id}</TableCell>
+                                                <TableCell align="left">{row.customer.name}</TableCell>
+                                                <TableCell align="left">{row.bookingDetails.vehicle.damageFee}</TableCell>
+                                                <TableCell align="left">
+                                                    <Tooltip title="Edit">
+                                                        <IconButton
+                                                            onClick={() => {
+                                                                this.updateDriver(row);
+                                                            }}
+                                                        >
+                                                            <EditIcon color="primary" />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                    <Tooltip title="Delete">
+                                                        <IconButton
+                                                            onClick={() => {
+                                                                this.deleteDriver(row.id)
+                                                            }}
+                                                        >
+                                                            <DeleteIcon color="error" />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                </TableCell>
+                                            </TableRow>
+                                        ))} */}
                                 </TableBody>
                             </Table>
                         </TableContainer>
